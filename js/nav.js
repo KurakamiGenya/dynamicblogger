edf1c35d20f38000b3c77f39255c7e9383c92713
Nguyễn Minh Hoàng <129413722+KurakamiGenya@users.noreply.github.com>
2024-09-11 00:33:47 +0700

    test path

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("../partials/nav.html")
+  fetch("/partials/nav.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("nav-placeholder").innerHTML = data;
